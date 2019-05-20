7630ac4479a68a7d5d15cb66b2fe7d3436629ae8
gatogarrido <fernando.garrido.rt@gmail.com>
2019-05-20 10:28:37 -0400

    Update ranking_lookup.R

--- ranking_lookup.R
+++ ranking_lookup.R
@@ -5,8 +5,9 @@ df <- readxl::read_xlsx("lista_universidades_base.xlsx")
 THE_rank <- readxl::read_xlsx("lista_rankings.xlsx", sheet = "Times_Higher_Ed")
 QS_rank <- readxl::read_xlsx("lista_rankings.xlsx", sheet = "QS_World")
 
-strdist_QS <- as.tibble(stringdistmatrix(df$Name, QS_rank$Name))
-strdist_QS <- strdist_QS %>% mutate(Name = df$Name) %>% select(Name, everything())
+strdist_QS <- as_tibble(stringdistmatrix(df$`names_list$Nombres`, QS_rank$Name))
+strdist_QS <- strdist_QS %>% mutate(Name = df$`names_list$Nombres`) %>% select(Name, everything())
 
 strdist_QS$min <- apply(strdist_QS, 1, min)
+
 writexl::write_xlsx(strdist_QS, "matrix.xlsx")
